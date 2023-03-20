cfb156bc1c6dffee0065b2aa3f3f11b9abb3886d
Evgenii Zemtsov <sever-zema@mail.ru>
2023-03-20 14:28:47 +0300

    Structure N-tier Architecture

--- customer-manager/src/main/java/com/zemtsov/Main.java
+++ customer-manager/src/main/java/com/zemtsov/Main.java
@@ -2,32 +2,11 @@ package com.zemtsov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-
-    @GetMapping("/greet")
-    public GreetResponse greet() {
-        return new GreetResponse("Hello",
-                List.of("Java", "SQL", "TypeScript"),
-                new Person("Eugene", 26, 100000));
-    }
-
-    record Person(String name, int age, double savings) {
-
-    };
-    record GreetResponse(String greet,
-                         List<String> favProgrammingLanguages,
-                         Person person) {
-
     }
 
 }
